63689fc65587fd2450d235b21acd1705391d94ce
neonritual <vtshore@gmail.com>
2021-08-15 20:58:01 +0900

    Create Hotels db

--- TabiNyans/admin.py
+++ TabiNyans/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Prefecture, City
+from .models import Prefecture, City, Hotel
 
+admin.site.register(Hotel)
 admin.site.register(Prefecture)
 admin.site.register(City)
--- TabiNyans/models.py
+++ TabiNyans/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from csv import DictReader
 import pandas as pd
@@ -15,16 +16,25 @@ class City(models.Model):
     def __str__(self):
         return self.city
 
-# with open('Prefectures.csv', 'r') as read_obj:
-#     csv_dict_reader = DictReader(read_obj)
-#     for row in csv_dict_reader:
-#             pref = row['prefecture_en']
-#             add_prefecture = Prefecture(prefecture=pref)
-#             add_prefecture.save()
+class Hotel(models.Model):
+    hotel_name = models.CharField(max_length=100, verbose_name='Hotel Name')
+    location_url = models.URLField(max_length=400, verbose_name='Google Maps URL', default='https://google.com')
+    prefecture = models.ForeignKey(Prefecture, on_delete=models.SET_NULL, blank=True, null=True)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=100, verbose_name='Hotel Name')
+    logo = models.ImageField(upload_to='logos/', verbose_name='Hotel Logo', null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    hours = models.CharField(max_length=100, verbose_name='Open Hours', blank=True)
+    price = models.CharField(max_length=100, verbose_name='Nightly Price', blank=True)
+    high_season = models.BooleanField(verbose_name='High Season Rates')
+    dogs = models.BooleanField(verbose_name='Dogs on Premises')
+    short_term = models.BooleanField(verbose_name='Short Term Available')
+    food = models.BooleanField(verbose_name='Food Available')
+    night_staff = models.BooleanField(verbose_name='24HR Staff')
+    specials = models.CharField(max_length=200, verbose_name='Special Services', blank=True)
+    extra = models.CharField(max_length=200, verbose_name='Other Information', blank=True)
+    images = models.ImageField(upload_to='hotel_image/', verbose_name='Hotel Image', blank=True, null=True)
+    admin_approved = models.BooleanField(default=False)
 
-# with open('Prefectures.csv', 'r') as read_obj:
-#     csv_dict_reader = DictReader(read_obj)
-#     for row in csv_dict_reader:
-#             pref = row['prefecture_en']
-#             print(pref)
-
+    def __str__(self):
+        return self.hotel_name
