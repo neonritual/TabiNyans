ea741dc569897fcc5ed2f6fd14229d3c4ddfad62
neonritual <vtshore@gmail.com>
2021-09-02 21:44:44 +0900

    Settings and Waitress

--- Tabi/wsgi.py
+++ Tabi/wsgi.py
@@ -13,4 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tabi.settings')
 
-application = get_wsgi_application()
+from whitenoise import WhiteNoise
+application = WhiteNoise(get_wsgi_application())
