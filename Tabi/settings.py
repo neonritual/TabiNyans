829d3847fe12305c9e3f85206661795117b849ff
neonritual <vtshore@gmail.com>
2021-09-03 23:57:31 +0900

    Switch DB to AWS (again)

--- Tabi/settings.py
+++ Tabi/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('TABINYANS_DJANGO')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost']
 # ALLOWED_HOSTS = ['.awsapprunner.com', 'localhost']
